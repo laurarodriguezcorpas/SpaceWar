001bea6ca8559e24c041a725f5e5cdc7ac18e400
pinksideofthemoon <31097119+pinksideofthemoon@users.noreply.github.com>
2019-05-21 23:18:44 +0200

    carpeta SRC actualizada

--- practica_2018-19/src/main/resources/static/src/states/menu.js
+++ practica_2018-19/src/main/resources/static/src/states/menu.js
@@ -6,7 +6,7 @@ Spacewar.menuState.prototype = {
 
 	init : function() {
 		if (game.global.DEBUG_MODE) {
-			console.log("[DEBUG] Entering **MENU** state");
+			console.log("[DEBUG JS] Entering **MENU** state");
 		}
 	},
 
@@ -25,16 +25,31 @@ Spacewar.menuState.prototype = {
 			game.global.socket.send(JSON.stringify(message))
 
 		}
+		
+		game.add.plugin(PhaserInput.Plugin);
+
+		
 	},
 	
 	create : function() {
 		background = game.add.tileSprite(0, 0, 1200, 800, 'background');
-		var buttonClassic = game.add.button(game.world.centerX - 85, 400, 'buttonStart', unirseARoom, this, 2, 1, 0);
-		buttonClassic.scale.setTo(0.5, 0.5);
+		var buttonStart = game.add.button(game.world.centerX - 85, 400, 'buttonStart', matchmaking, this, 2, 1, 0);
+		buttonStart.scale.setTo(0.5, 0.5);
 		
+		var password = game.add.inputField(10, 90, {
+		    font: '18px Arial',
+		    fill: '#212121',
+		    fontWeight: 'bold',
+		    width: 150,
+		    padding: 8,
+		    borderWidth: 1,
+		    borderColor: '#000',
+		    borderRadius: 6,
+		    placeHolder: 'NOMBRA A TU TROPitA: ',
+		});
 		
-		function unirseARoom () {
-			game.state.start('gameState')
+		function matchmaking () {
+			game.state.start('gameModeState')
 		}
 	},
 
